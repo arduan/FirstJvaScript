24a0a56dd7cfccdcda7385f5c56e4cfbd7640cee
Vitaly <ivanov.arduan@yandex.ru>
2021-10-26 18:46:47 +0300

    Исправил ошибку и поменял var на let.

--- playlist.js
+++ playlist.js
@@ -5,15 +5,15 @@
 window.onload = init;
 
 function init(){
-    var button = document.getElementById("addButton");
+    let button = document.getElementById("addButton");
     button.onclick = handleButtonClick;
 }
 
 function handleButtonClick() {
-    var textInput = document.getElementById("songTextInput");
-    var songName = textInput.valule;
-    var li = document.createElement("li");
+    let textInput = document.getElementById("songTextInput");
+    let songName = textInput.value;
+    let li = document.createElement("li");
     li.innerHTML = songName;
-    var ul = document.getElementById("playlist");
+    let ul = document.getElementById("playlist");
     ul.appendChild(li)
 }
